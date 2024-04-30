a054527358678cb743e148ec2895ca6ad3f0436f
jpbroering <102531849+jpbroering@users.noreply.github.com>
2024-04-30 11:28:16 -0300

    iniciei o projeto

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author 10724112106
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        visao.FRMSorteio janela = new visao.FRMSorteio();
+        janela.setVisible(true);
+    }
+}
